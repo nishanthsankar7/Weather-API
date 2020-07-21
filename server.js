3f98639728fd9cf939cc86f30bdbfbe922dadd17
nishanthsankar7 <nishanthsankar207@gmail.com>
2020-07-21 12:06:57 +0530

    env commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const https = require('https')
 const bodyParser = require('body-parser')
@@ -15,7 +16,7 @@ app.post('/',function(req,res){
   console.log(req.body.city)
 
   var city = req.body.city
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=c934ccb22d2a6f5979706c11afa2c5bd&units=metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city + process.env.ID
   https.get(url,function(response){
 
 
@@ -25,7 +26,7 @@ app.post('/',function(req,res){
       const temp = weatherData.main.temp
       const description = weatherData.weather[0].description
       const icon =weatherData.weather[0].icon
-      
+
       res.write('the temp in '+city+' is = ' + temp + ' degree celcius')
       res.write('\n' + icon)
       res.send()
